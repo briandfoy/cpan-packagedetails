64242a870f32f7d0776ece626f3dec6ad94b1345
brian d foy <brian.d.foy@gmail.com>
2009-08-17 15:18:09 -0500

    * Improve _filter_old_dists by watching for order of additions
    I was pretty stupid with the previous way I did this. I tracked
    everything in %Seen, put returned the list to the next map anyway.
    If the older dist showed up in the list first, I passed it on because
    I hadn't seen anything newer yet. The list order matters.
    
    I don't completely like how I've done it now, but it works.

--- lib/PackageDetails.pm
+++ lib/PackageDetails.pm
@@ -9,7 +9,7 @@ use Carp qw(carp croak cluck confess);
 use File::Spec::Functions;
 
 BEGIN {
-	$VERSION = '0.21_04';
+	$VERSION = '0.21_05';
 	}
 
 =head1 NAME
@@ -493,26 +493,35 @@ sub _filter_older_dists
 	require CPAN::DistnameInfo;
 	
 	my %Seen;
+	my @order;
 	
-	my @temp = map { # extract for the hash
-			$_->[0]
+	foreach my $path ( @$array )
+		{
+		my $distname = CPAN::DistnameInfo->new($path);
+		my( $name, $version ) = map { $distname->$_ } qw(dist version);
+		my $tuple = [ $path, $name, $version ];
+		push @order, $name;
+		
+		   # first branch, haven't seen the distro yet
+		   if( ! exists $Seen{ $name } )       { $Seen{ $name } = $tuple }
+		   # second branch, the version we see now is greater than before
+		elsif( $Seen{ $name }[2] < $version )  { $Seen{ $name } = $tuple }
+		   # third branch, nothing. Really? Are you sure there's not another case?
+		else                                   { () }
+		}
+		
+	@$array = map { 
+		if( exists $Seen{$_} )
+			{
+			my $dist = $Seen{$_}[0];
+			delete $Seen{$_};
+			$dist;
 			}
-		map { 
-			   # first branch, we've never seen this distro name
-			   if( ! exists $Seen{ $_->[1] } )     { $Seen{$_->[1]} = $_ }
-			   # second branch, the version we see now is greater than before
-			elsif( $Seen{ $_->[1] }[2] < $_->[2] ) { $Seen{$_->[1]} = $_ }
-			   # third branch, nothing. Really? Are you sure there's not another case?
-			else                                   { () }
+		else
+			{
+			()
 			}
-		map { # create the tuples
-			my $path = $_;
-			my $distname = CPAN::DistnameInfo->new($path);
-			my( $name, $version ) = map { $distname->$_ } qw(dist version);
-			[ $path, $name, $version ];
-			} @$array;
-	
-	@$array = @temp;
+		} @order;
 	
 	return 1;
 	}
--- t/_filter_older_dists.t
+++ t/_filter_older_dists.t
@@ -12,6 +12,7 @@ use_ok( $class );
 can_ok( $class, $method );
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# try it with relative paths and no older dists
 {
 my @list = qw( 
 	Foo-1.23.tgz 
@@ -26,6 +27,7 @@ is_deeply( \@copy, \@list, "Unique list has the same elements it started with" )
 }
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Try it with relative paths and and an older dist
 {
 my @list = qw( 
 	Foo-1.23.tgz
@@ -41,6 +43,24 @@ my @expected = qw(
 	);
 
 $class->$method( \@list );
-is( scalar @list, scalar @expected, "Filtered list has the right length" );
-is_deeply( \@list, \@expected );
+is( scalar @list, scalar @expected, "Filtered list of filenames has the right length" );
+is_deeply( \@list, \@expected, "Filtered list of filenames paths has right paths" );
+}
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Try it with absolute paths and and an older dist
+# The older dist comes first in the list
+{
+my @list = qw( 
+	/a/b/c/d/authors/id/B/BDFOY/Test-Data-1.21.tgz
+	/a/b/c/d/authors/id/B/BDFOY/Test-Data-1.22.tgz
+	);
+
+my @expected = qw( 
+	/a/b/c/d/authors/id/B/BDFOY/Test-Data-1.22.tgz
+	);
+
+$class->$method( \@list );
+is( scalar @list, scalar @expected, "Filtered list of absolute paths has the right length" );
+is_deeply( \@list, \@expected, "Filtered list of absolute paths has right paths" );
 }
